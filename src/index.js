d62099e30d8acb7b3261fafc19c593ef1fb257a7
blake <blake.netzeband@gmail.com>
2019-04-29 18:10:11 -0600

    added error page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-// for Magic heading
-const $magicEl = document.querySelector(".magic .quote");
-$magicEl.parentNode.style.height = $magicEl.offsetHeight + "px";
+document.addEventListener("DOMContentLoaded", () => init(true));
+window.addEventListener("resize", () => init(false));
+
+function init(shouldUnregister) {
+  magic();
+  if (shouldUnregister) {
+    stopListening();
+  }
+}
+
+function stopListening() {
+  document.removeEventListener("DOMContentLoaded", init);
+}
+
+// js for magic section
+function magic() {
+  // i hate this but have to wait till style is applied
+  const $magicEl = document.querySelector(".magic .quote");
+  $magicEl.parentNode.style.height = $magicEl.offsetHeight + "px";
+}
