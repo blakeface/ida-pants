311279f3b228be15513a46dc5592ff100a3c4fa5
netzebandito <blake.netzeband@gmail.com>
2019-05-01 20:28:39 -0600

    restore prev changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-// for Magic heading
-const $magicEl = document.querySelector(".magic .quote");
-$magicEl.parentNode.style.height = $magicEl.offsetHeight + "px";
+window.onload = function() {
+	// for Magic heading
+	const $magicEl = document.querySelector(".magic .quote");
+	const $magicAuthor = document.querySelector(".magic .author");
+	$magicEl.parentNode.style.height = $magicEl.offsetHeight + "px";
+	$magicAuthor.classList.remove("hidden");
+};
